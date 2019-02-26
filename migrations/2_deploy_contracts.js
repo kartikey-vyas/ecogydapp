3aabfd7ade22314ee3265ede9c22587bb3898358
kartikey-vyas <kartikeyvyas23@gmail.com>
2019-02-26 17:07:19 +1300

    testing oraclize locally

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,9 +3,13 @@ var usingOraclize = artifacts.require("./usingOraclize.sol");
 var EcogyOracle = artifacts.require("./EcogyOracle.sol");
 var Ecogy = artifacts.require("./Ecogy.sol");
 
-module.exports = function (deployer) {
+module.exports = function (deployer, network, accounts) {
   deployer.deploy(SafeMath);
   deployer.deploy(usingOraclize);
   deployer.deploy(EcogyOracle);
-  deployer.deploy(Ecogy);
+  deployer.deploy(Ecogy, {
+    from: accounts[0],
+    gas: 6721975,
+    value: 500000000000000000
+  });
 };
